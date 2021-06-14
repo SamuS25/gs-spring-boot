8b2f0073f300fad5fb98744c1fd3c1816d132cca
EC2 Default User <ec2-user@ip-172-31-22-130.eu-west-3.compute.internal>
2021-06-14 10:48:54 +0000

    Volviendo a probar que travis funcione

--- test/run.sh
+++ test/run.sh
@@ -4,10 +4,15 @@ cd $(dirname $0)
 cd ../complete
 
 ./gradlew check
+ret=$?
+if [ $ret -ne 0 ]; then
+    exit $ret
+fi
+
 ./gradlew test --rerun-tasks --stacktrace
 ret=$?
 if [ $ret -ne 0 ]; then
-exit $ret
+    exit $ret
 fi
 rm -rf build
 
